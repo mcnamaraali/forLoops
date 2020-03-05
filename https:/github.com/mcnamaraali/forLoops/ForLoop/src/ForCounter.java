f832a6dc0358da7083c5326a55dd3d83ad4fecae
alimcnamara <mcnamaraali@gmail.com>
2020-03-05 19:49:06 +0100

    For counter

--- https:/github.com/mcnamaraali/forLoops/ForLoop/src/ForCounter.java
+++ https:/github.com/mcnamaraali/forLoops/ForLoop/src/ForCounter.java
@@ -4,10 +4,13 @@ public class ForCounter {
 
 	public static void main(String[] args) {
 		
+		//initialisation ; required semicolon ;loop-continuation condition; incrementation of the control variable
 		for (int counter = 1; counter <= 10; counter++) {
 			System.out.printf("%d ", counter);
 		}
 		System.out.println();
+		
+		
 		
 	}
 
